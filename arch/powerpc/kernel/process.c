61a001ee0462d2c329b71e3eb8d6b59a9a806e9f
Michael Neuling <mikey@neuling.org>
2021-04-22 12:28:56 +1000

    fix

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -1887,8 +1887,10 @@ void start_thread(struct pt_regs *regs, unsigned long start, unsigned long sp)
 	current->thread.tm_tfiar = 0;
 	current->thread.load_tm = 0;
 #endif /* CONFIG_PPC_TRANSACTIONAL_MEM */
-	if (cpu_has_feature(CPU_FTR_ARCH_31))
+	if (cpu_has_feature(CPU_FTR_ARCH_31)) {
 		current->thread.hashkeyr = get_random_long();
+		mtspr(SPRN_HASHKEYR, current->thread.hashkeyr);
+	}
 
 }
 EXPORT_SYMBOL(start_thread);
